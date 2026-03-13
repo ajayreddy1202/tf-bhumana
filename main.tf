aa86a680c1a7ac662e3bccaa51a541074d166674
ajayreddy1202 <ajayreddyvishnu@gmail.com>
2026-03-13 15:39:57 +0530

    my first commit

---
+++ main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_instance" "my_server" {
+  count         = 1
+  ami           = "ami-0f559c3642608c138"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "bhumana-tf-server"
+  }
+}
